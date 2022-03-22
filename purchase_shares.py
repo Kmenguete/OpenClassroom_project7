223d5bfbc2e2528279c23e7bea069564b4a4f0c6
Kmenguete <menguetekevin@gmail.com>
2022-03-22 17:35:06 +0100

    defining purchase shares function/method

---
+++ purchase_shares.py
@@ -0,0 +1,12 @@
+
+
+def purchase_shares(purchase_value, number_of_shares):
+
+    while purchase_value < 500:
+
+        number_of_shares += 1
+
+    else:
+
+        print("We spent the maximum amount of shares per customers(500 euros)")
+
