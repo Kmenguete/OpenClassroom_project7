c3fce7257afd8c08e0913d8bbc52bb99e8003c69
Kmenguete <menguetekevin@gmail.com>
2022-03-23 15:41:51 +0100

    trying to get a global report of my dataframe

--- bruteforce.py
+++ bruteforce.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import pandas_profiling as pp
 
 shares_dataframe1 = pd.read_csv('dataset1_Python+P7.csv')
 
 shares_dataframe1_top = shares_dataframe1.head(5)
 
-print(shares_dataframe1_top)
+shares_dataframe1_report = pp.ProfileReport(shares_dataframe1)
+
+print(shares_dataframe1_report)
