0c7034315b68a490df235a0c5c81d5c39b5e8608
Kmenguete <menguetekevin@gmail.com>
2022-03-23 10:05:03 +0100

    refactoring purchase shares method

--- list_shares.py
+++ list_shares.py
@@ -3,4 +3,15 @@ class ListShares:
 
     def __init__(self, shares_list):
         self.shares_list = shares_list
+
+    @staticmethod
+    def purchase_shares(purchase_value, share):
+
+        while purchase_value < 500:
+
+            share += 1
+
+        else:
+
+            print("We spent the maximum amount of shares per customers(500 euros)")
         
