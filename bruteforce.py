4b3923710f3845abea2c0c54cc18c0408b237031
Kmenguete <menguetekevin@gmail.com>
2022-03-23 14:38:24 +0100

    successfully checked my dataframe

--- bruteforce.py
+++ bruteforce.py
@@ -2,4 +2,6 @@ import pandas as pd
 
 shares_dataframe1 = pd.read_csv('dataset1_Python+P7.csv')
 
-shares_dataframe1.head()
+shares_dataframe1_top = shares_dataframe1.head(5)
+
+print(shares_dataframe1_top)
