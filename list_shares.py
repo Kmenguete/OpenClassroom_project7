982e44810c471956f44d7b9b3b591471ae55f3e7
Kmenguete <menguetekevin@gmail.com>
2022-03-23 10:23:46 +0100

    I am preventing the possibility to buy a share twice

--- list_shares.py
+++ list_shares.py
@@ -15,4 +15,14 @@ class ListShares:
         else:
 
             print("We spent the maximum amount of shares per customers(500 euros)")
+
+    def prevent_duplicated_share(self, share_index, purchase_value):
+
+        if self.shares_list[share_index] in self.shares_list:
+
+            pass
+
+        else:
+
+            self.purchase_shares(purchase_value, self.shares_list[share_index])
         
