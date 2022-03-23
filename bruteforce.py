9669902dd3e43d16368b707b076235ef96f6dbc3
Kmenguete <menguetekevin@gmail.com>
2022-03-23 14:20:34 +0100

    giving a more appropriate name to my dataframe

--- bruteforce.py
+++ bruteforce.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-shares_dataset1 = pd.read_csv('dataset1_Python+P7.csv')
+shares_dataframe1 = pd.read_csv('dataset1_Python+P7.csv')
 
-shares_dataset1.head()
+shares_dataframe1.head()
