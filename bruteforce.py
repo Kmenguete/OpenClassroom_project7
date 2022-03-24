d14bf9fc2f1d6a7b845852d0e4c8d8c306affb30
Kmenguete <menguetekevin@gmail.com>
2022-03-24 14:57:28 +0100

    I evaluated wether a positive correlation exists between the cost of a share and its profitability?

--- bruteforce.py
+++ bruteforce.py
@@ -13,3 +13,10 @@ import numpy as np
 shares_dataframe1 = pd.read_csv('dataset1_Python+P7.csv')
 
 color = sn.color_palette()
+
+figure = px.scatter(shares_dataframe1, x='profit', y='price')
+figure.update_traces(marker_color="rgb(158,11,200)", marker_line_color="rgb(15,179,104)", marker_line_width=1.5)
+figure.update_layout(title_text='Is there a positive correlation between the cost of a share and its profitability?')
+relationship_cost_profitability = figure.show()
+
+print(relationship_cost_profitability)
