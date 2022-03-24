e560f1c6200e84137d77d7bfac9a22b9d393b795
Kmenguete <menguetekevin@gmail.com>
2022-03-24 13:57:44 +0100

    I don't use jupyter notebook for this project

--- bruteforce.py
+++ bruteforce.py
@@ -9,12 +9,10 @@ import plotly.offline as py
 import plotly.tools as tls
 import plotly.express as px
 import numpy as np
-from tqdm.autonotebook import get_ipython
+
 
 shares_dataframe1 = pd.read_csv('dataset1_Python+P7.csv')
 
 color = sn.color_palette()
-
-get_ipython().run_line_magic('matplotlib', 'inline')
 
 py.init_notebook_mode(connected=True)
