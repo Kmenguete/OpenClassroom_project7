f604576b89207f524b802ea3f71db4fdc675ab29
Kmenguete <menguetekevin@gmail.com>
2022-03-24 15:03:53 +0100

    explaining results of my dataset analysis in comments

--- bruteforce.py
+++ bruteforce.py
@@ -1,13 +1,8 @@
 import pandas as pd
 
 # Visualization Imports
-
-import matplotlib.pyplot as plt
 import seaborn as sn
-import plotly.graph_objs as go
-import plotly.tools as tls
 import plotly.express as px
-import numpy as np
 
 
 shares_dataframe1 = pd.read_csv('dataset1_Python+P7.csv')
@@ -19,4 +14,7 @@ figure.update_traces(marker_color="rgb(158,11,200)", marker_line_color="rgb(15,1
 figure.update_layout(title_text='Is there a positive correlation between the cost of a share and its profitability?')
 relationship_cost_profitability = figure.show()
 
-print(relationship_cost_profitability)
+# print(relationship_cost_profitability)
+
+# results of my dataset analysis: There is not any relationship between cost of shares and their profitability.
+# Finding the list of most profitable shares is a lottery.
