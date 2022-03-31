5ef3263aab12202ceee76138db786c3f3f54fa1a
Kmenguete <menguetekevin@gmail.com>
2022-03-31 10:59:19 +0200

    Finally I will directly work on bruteforce solution the gluttonous solution will be useless

--- bruteforce.py
+++ bruteforce.py
@@ -75,26 +75,3 @@ def add_column_in_csv(input_file, output_file, transform_row):
 
 
 sorted_shares_by_real_profit = bruteforce_shares_real_profit.sort_values('real profit')
-
-
-def get_most_profitable_shares(shares_list, max_cost):
-    sorted_shares_list = shares_list.sort_values('real profit')
-    total_cost = 0
-    most_profitable_shares = pd.DataFrame(columns=['name', 'price', 'profit', 'real profit'])
-    i = 0
-    while i < len(sorted_shares_list) and total_cost < max_cost:
-        cost_share = get_price_of_share(sorted_shares_list, i)
-        if total_cost + cost_share <= max_cost:
-            most_profitable_shares.append({'name': sorted_shares_list.at[i, 'name'],
-                                           'price': sorted_shares_list.at[i, 'price'],
-                                           'profit': sorted_shares_list.at[i, 'profit'],
-                                           'real profit': sorted_shares_list.at[i, 'profit']}, ignore_index=True)
-            total_cost = total_cost + cost_share
-        else:
-            pass
-        i = i + 1
-    print(most_profitable_shares)
-    return most_profitable_shares
-
-
-get_most_profitable_shares(bruteforce_shares_real_profit, 500)
