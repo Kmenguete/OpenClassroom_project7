eb24e53094fdc49e415d8b9208353a7f739ef174
Kmenguete <menguetekevin@gmail.com>
2022-04-19 16:39:09 +0200

    Finally I fixed the issue of adding a new column to my csv file

--- optimized_solution.py
+++ optimized_solution.py
@@ -1,4 +1,3 @@
-import csv
 import pandas as pd
 
 shares_dataframe1 = pd.read_csv('dataset1_Python+P7.csv')
@@ -8,24 +7,18 @@ csv_dataset1 = 'dataset1_Python+P7.csv'
 csv_dataset1_real_profit = 'dataset1_Python+P7_real_profit.csv'
 
 
-def get_real_profit(shares, csv_shares, csv_shares_real_profit):
-    real_profit_list = ['real profit', ]
+def get_real_profit(shares, csv_shares_real_profit):
+    real_profit_list = []
     for i in range(len(shares)):
         real_profit = shares.loc[i, 'price'] * (shares.loc[i, 'profit'] / 100)
         real_profit_list.append(real_profit)
-    add_column_in_csv(csv_shares, csv_shares_real_profit, real_profit_list)
+    add_column_in_csv(shares, csv_shares_real_profit, real_profit_list)
 
 
 def add_column_in_csv(input_file, output_file, real_profit_list):
-    with open(input_file, 'r') as csv_input, open(output_file, 'w') as csv_output:
-        writer = csv.writer(csv_output, lineterminator='\n')
-        reader = csv.reader(csv_input)
-        row = next(reader)
-        real_profit_list.append(row)
-        for row in reader:
-            row.append(row[0])
-            real_profit_list.append(row)
-        writer.writerow(real_profit_list)
+    csv_input = input_file
+    csv_input['real profit'] = real_profit_list
+    csv_input.to_csv(output_file, index=False)
 
 
-get_real_profit(shares_dataframe1, csv_dataset1, csv_dataset1_real_profit)
+get_real_profit(shares_dataframe1, csv_dataset1_real_profit)
