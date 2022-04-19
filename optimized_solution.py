f4f28afb0e8dbe4aabe916938732a8881062cd0b
Kmenguete <menguetekevin@gmail.com>
2022-04-19 16:14:58 +0200

    still looking for a way to add real profit data to my csv file

--- optimized_solution.py
+++ optimized_solution.py
@@ -13,22 +13,20 @@ def get_real_profit(shares, csv_shares, csv_shares_real_profit):
     for i in range(len(shares)):
         real_profit = shares.loc[i, 'price'] * (shares.loc[i, 'profit'] / 100)
         real_profit_list.append(real_profit)
-    add_column_in_csv(csv_shares, csv_shares_real_profit)
+    add_column_in_csv(csv_shares, csv_shares_real_profit, real_profit_list)
 
 
-def add_column_in_csv(input_file, output_file):
+def add_column_in_csv(input_file, output_file, real_profit_list):
     with open(input_file, 'r') as csv_input:
         with open(output_file, 'w') as csv_output:
             writer = csv.writer(csv_output, lineterminator='\n')
             reader = csv.reader(csv_input)
-            new_column = []
             row = next(reader)
-            row.append('real profit')
-            new_column.append(row)
+            real_profit_list.append(row)
             for row in reader:
                 row.append(row[0])
-                new_column.append(row)
-            writer.writerow(new_column)
+                real_profit_list.append(row)
+            writer.writerow(real_profit_list)
 
 
 get_real_profit(shares_dataframe1, csv_dataset1, csv_dataset1_real_profit)
