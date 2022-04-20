1f13cd650a3e8482a7f8c2c52d45eb672eb00c9f
Kmenguete <menguetekevin@gmail.com>
2022-04-20 14:14:43 +0200

    I am reorganizing my solution

--- optimized_solution.py
+++ optimized_solution.py
@@ -61,20 +61,17 @@ def get_most_profitable_shares(shares):
     minimum_real_profit_amount = shares['real profit'].quantile(0.9)
     # From the dataframe of the dataset I will create a smaller dataframe that respect the requirements of customers.
     most_profitable_shares_list = pd.DataFrame(columns=['name', 'price', 'profit', 'real profit'])
-    total_price_of_shares = 0
     i = 0
     real_profit_of_shares = get_real_profit_of_share(shares, i)
     while real_profit_of_shares >= minimum_real_profit_amount:
         most_profitable_shares_list = most_profitable_shares_list.append({'name': shares.at[i, 'name'],
                                                                           'price': shares.at[i, 'price'],
                                                                           'profit': shares.at[i, 'profit'],
-                                                                          'real profit': shares.at[
-                                                                              i, 'real profit']},
+                                                                          'real profit': shares.at[i, 'real profit']},
                                                                          ignore_index=True)
-        total_price_of_shares = total_price_of_shares + shares.at[i, 'price']
         i += 1
     else:
-        print("Every shares that are among the 10 % most profitable has been browsed.")
+        print("Every shares that are among the 10 % most profitable shares has been browsed.")
 
 
 def get_500_euros_shares_list(shares):
