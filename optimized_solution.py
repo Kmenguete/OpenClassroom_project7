b7d5cc5c1429c4130477c2a9e53e30ffc18249ed
Kmenguete <menguetekevin@gmail.com>
2022-04-20 13:03:22 +0200

    retrieving useful functions from bruteforce algorithm

--- optimized_solution.py
+++ optimized_solution.py
@@ -9,6 +9,34 @@ csv_dataset1_real_profit = 'dataset1_Python+P7_real_profit.csv'
 shares_dataframe1_real_profit = pd.read_csv('dataset1_Python+P7_real_profit.csv')
 
 
+def get_price_of_share(shares, index):
+    print(shares.at[index, 'price'])
+    return shares.at[index, 'price']
+
+
+def get_real_profit_of_share(shares, index):
+    print(shares.at[index, 'real profit'])
+    return shares.at[index, 'real profit']
+
+
+def get_total_price_of_shares(shares):
+    total_price_of_shares = shares['price'].sum()
+    print(total_price_of_shares)
+    return total_price_of_shares
+
+
+def get_total_profit_of_shares(shares):
+    total_profit_of_shares = shares['profit'].sum()
+    print(total_profit_of_shares)
+    return total_profit_of_shares
+
+
+def get_total_real_profit_of_shares(shares):
+    total_real_profit_of_shares = shares['real profit'].sum()
+    print(total_real_profit_of_shares)
+    return total_real_profit_of_shares
+
+
 def get_real_profit(shares, csv_shares_real_profit):
     real_profit_list = []
     for i in range(len(shares)):
@@ -21,6 +49,24 @@ def add_column_in_csv(input_file, output_file, real_profit_list):
     csv_input = input_file
     csv_input['real profit'] = real_profit_list
     csv_input.to_csv(output_file, index=False)
+
+
+def get_500_euros_shares_list(shares):
+    maximum_total_price_shares = 500
+    # I will first slice my dataframe in smaller dataframe with maximum total price of 500 euros
+    sample_dataframe = pd.DataFrame(columns=['name', 'price', 'profit', 'real profit'])
+    total_price_of_shares = 0
+    i = 0
+    while total_price_of_shares <= maximum_total_price_shares:
+        sample_dataframe = sample_dataframe.append({'name': shares.at[i, 'name'], 'price': shares.at[i, 'price'],
+                                                    'profit': shares.at[i, 'profit'],
+                                                    'real profit': shares.at[i, 'real profit']},
+                                                   ignore_index=True)
+        total_price_of_shares = total_price_of_shares + shares.at[i, 'price']
+        i += 1
+    else:
+        print("You reached the maximum authorized total price cost.")
+    print(sample_dataframe)
 
 
 """
