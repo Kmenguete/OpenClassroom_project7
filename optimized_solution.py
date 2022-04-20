454c3fc036976be38c6c1f636bcd4e6977af463d
Kmenguete <menguetekevin@gmail.com>
2022-04-20 13:09:35 +0200

    Keeping only functions that I may use for my optimized solution algorithm

--- optimized_solution.py
+++ optimized_solution.py
@@ -15,12 +15,6 @@ def get_total_price_of_shares(shares):
     return total_price_of_shares
 
 
-def get_total_profit_of_shares(shares):
-    total_profit_of_shares = shares['profit'].sum()
-    print(total_profit_of_shares)
-    return total_profit_of_shares
-
-
 def get_total_real_profit_of_shares(shares):
     total_real_profit_of_shares = shares['real profit'].sum()
     print(total_real_profit_of_shares)
@@ -41,24 +35,6 @@ def add_column_in_csv(input_file, output_file, real_profit_list):
     csv_input.to_csv(output_file, index=False)
 
 
-def get_500_euros_shares_list(shares):
-    maximum_total_price_shares = 500
-    # I will first slice my dataframe in smaller dataframe with maximum total price of 500 euros
-    sample_dataframe = pd.DataFrame(columns=['name', 'price', 'profit', 'real profit'])
-    total_price_of_shares = 0
-    i = 0
-    while total_price_of_shares <= maximum_total_price_shares:
-        sample_dataframe = sample_dataframe.append({'name': shares.at[i, 'name'], 'price': shares.at[i, 'price'],
-                                                    'profit': shares.at[i, 'profit'],
-                                                    'real profit': shares.at[i, 'real profit']},
-                                                   ignore_index=True)
-        total_price_of_shares = total_price_of_shares + shares.at[i, 'price']
-        i += 1
-    else:
-        print("You reached the maximum authorized total price cost.")
-    print(sample_dataframe)
-
-
 """
 I envision to build a recommender system for the optimized solution. I will build an algorithm that will recommend
 the most profitable shares list for a total maximum cost of shares of 500 euros.
@@ -75,7 +51,3 @@ total cost of 500 euros.
 My program should pick shares with highest real profit, store it into a list and the total cost of shares should 
 not exceed 500 euros.
 """
-
-average_real_profit = shares_dataframe1_real_profit['real profit'].mean()
-
-# print("Here is the average real profit of our dataset 1: " + str(average_real_profit))
