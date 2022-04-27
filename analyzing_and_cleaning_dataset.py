8b14245a9613e9fc38128a0ecee1009976ca0527
Kmenguete <menguetekevin@gmail.com>
2022-04-27 13:17:22 +0200

    successfully cleaned dataset2

--- analyzing_and_cleaning_dataset.py
+++ analyzing_and_cleaning_dataset.py
@@ -1,17 +1,17 @@
 import pandas as pd
 import seaborn as sns
 
-# import plotly.express as px
+import plotly.express as px
 
 
 color = sns.color_palette()
-shares_dataframe2 = pd.read_csv('dataset2_Python+P7.csv')
+shares_dataframe2 = pd.read_csv('dataset2.csv')
 
 
-# fig = px.scatter(shares_dataframe2, x='price', y='profit')
-# fig.update_traces(marker_color="rgb(100,0,149)", marker_line_color="rgb(215,193,80)", marker_line_width=1.5)
-# fig.update_layout(title_text='relationship between cost and profitability for dataset2')
-# fig.show()
+fig = px.scatter(shares_dataframe2, x='price', y='profit')
+fig.update_traces(marker_color="rgb(100,0,149)", marker_line_color="rgb(215,193,80)", marker_line_width=1.5)
+fig.update_layout(title_text='relationship between cost and profitability for dataset2')
+fig.show()
 
 
 def remove_outliers(shares):
@@ -23,7 +23,8 @@ def remove_outliers(shares):
                                                       'profit': shares.at[i, 'profit']}, ignore_index=True)
         else:
             print("The price of this share does not make sense: " + str(price_of_share))
+    clean_dataframe.to_csv('dataset2.csv', index=False)
     print(clean_dataframe.to_string())
 
 
-remove_outliers(shares_dataframe2)
+# remove_outliers(shares_dataframe2)
