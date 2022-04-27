3964afb8f3f2b56b93fac02ed9a543d11876eac8
Kmenguete <menguetekevin@gmail.com>
2022-04-27 13:12:22 +0200

    removing outliers from dataset2

--- analyzing_and_cleaning_dataset.py
+++ analyzing_and_cleaning_dataset.py
@@ -15,8 +15,15 @@ shares_dataframe2 = pd.read_csv('dataset2_Python+P7.csv')
 
 
 def remove_outliers(shares):
-    shares.drop([shares.loc[shares['price'] < 0]])
-    print(shares.to_string())
+    clean_dataframe = pd.DataFrame(columns=['name', 'price', 'profit'])
+    for i in range(0, len(shares)):
+        price_of_share = shares.at[i, 'price']
+        if price_of_share >= 0:
+            clean_dataframe = clean_dataframe.append({'name': shares.at[i, 'name'], 'price': shares.at[i, 'price'],
+                                                      'profit': shares.at[i, 'profit']}, ignore_index=True)
+        else:
+            print("The price of this share does not make sense: " + str(price_of_share))
+    print(clean_dataframe.to_string())
 
 
 remove_outliers(shares_dataframe2)
