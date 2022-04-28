3d6b15596c0e08e01d52299e0224db1ee5ac345b
Kmenguete <menguetekevin@gmail.com>
2022-04-28 10:26:19 +0200

    I will compute the total number of bytes used by the algorithm according the number of inputs data

--- data.py
+++ data.py
@@ -1,3 +1,5 @@
+import sys
+
 
 # Retrieve the data from the 20 shares list for the brute force.
 data = [
@@ -22,3 +24,7 @@ data = [
     {"name": "action19", "cout": 24, "benefice": 21},
     {"name": "action20", "cout": 114, "benefice": 18}
 ]
+
+data_size = sys.getsizeof(data)
+
+print("Here is the size of data list object in memory: " + str(data_size) + " bytes")
