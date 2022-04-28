12e48c204e3face1e2e6f59ddec6e949e8ac6517
Kmenguete <menguetekevin@gmail.com>
2022-04-28 13:29:09 +0200

    I will compute the size of optimized solution algorithm in memory

---
+++ computation_of_optimized_solution_size.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import sys
+
+
+shares_dataframe1 = pd.read_csv('dataset1_Python+P7.csv')
+
+shares_dataframe2 = pd.read_csv('dataset2.csv')
+
+csv_dataset1 = 'dataset1_Python+P7.csv'
+
+csv_dataset2 = 'dataset2.csv'
+
+csv_dataset2_real_profit = 'dataset2_real_profit.csv'
+
+csv_dataset1_real_profit = 'dataset1_Python+P7_real_profit.csv'
+
+shares_dataframe1_real_profit = pd.read_csv('dataset1_Python+P7_real_profit.csv')
+
+shares_dataframe2_real_profit = pd.read_csv('dataset2_real_profit.csv')
+
+shares_dataframe1_size = sys.getsizeof(shares_dataframe1)
+
+shares_dataframe2_size = sys.getsizeof(shares_dataframe2)
